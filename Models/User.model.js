fc501d99b284e1438d66ccd40ed463886a972270
khainpkimei <khainp@kimei.vn>
2021-12-21 17:18:27 +0700

    Connect and create model using mongoose in Node.js

---
+++ Models/User.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema();
+
+const UserSchema = new schema({
+    username: {
+        type: String,
+        lowercase: true,
+        unique: true,
+        require: true
+    },
+    password: {
+        type: String,
+        require: true
+    }
+});
+
+module.exports = mongoose.model('user',UserSchema);
